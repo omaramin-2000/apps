a1f65baf4b82150ef95ad407601a1870e0faafcf
Michael Hansen <mike@rhasspy.org>
2026-08-28 16:41:10 -0500

    Remove experimental voice app

--- voice/script/build_proto.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# Requires protoc to be installed
-
-this_dir="$( cd "$( dirname "$0" )" && pwd )"
-src_dir="$(realpath "${this_dir}/../src")"
-
-protoc -I "${src_dir}/protos/" "--python_betterproto2_out=${src_dir}/voice_messages" "${src_dir}/protos/voice.proto"
